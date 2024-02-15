599ca9c552d8f97230b15b60a39cff285c134646
g6nfm <fahey27j@gmail.com>
2024-02-15 16:30:22 -0500

    Home Page Started
    have to leave it here for now, taking off for the evening. Might have time for it later. - Jakob

--- SJHAFitness/SJHAFitness/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/SJHAFitness.AssemblyInfo.cs
+++ SJHAFitness/SJHAFitness/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/SJHAFitness.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SJHAFitness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3bb62a4ab52158652a4fe7608e06e6e63503530f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+87fb2177922bcb530370437fe364fa347db8a340")]
 [assembly: System.Reflection.AssemblyProductAttribute("SJHAFitness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SJHAFitness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
