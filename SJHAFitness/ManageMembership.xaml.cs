51b8dc4a8999df658a80030ef95c31924e3b17d8
g6nfm <fahey27j@gmail.com>
2024-03-31 22:48:01 -0400

    Push for the evening

--- SJHAFitness/ManageMembership.xaml.cs
+++ SJHAFitness/ManageMembership.xaml.cs
@@ -11,7 +11,25 @@ public partial class ManageMembership : ContentPage
 {
     ObservableCollection<Sessions> Session;
 
-    async void OnMembershipClicked(object sender, EventArgs e)
+    private async void OnCancelMembershipClicked(object sender, EventArgs e)
+    {
+        // Ask the user for confirmation
+        bool isUserSure = await DisplayAlert("Cancel Membership", "Are you sure you'd like to cancel your membership?", "Yes", "No");
+
+        if (isUserSure)
+        {
+            // logic for removing account from database will be here
+
+            // Show a confirmation message
+            await DisplayAlert("Membership Cancelled", "Your membership has been cancelled.", "OK");
+
+            
+            // await Navigation.PopAsync(); // navigate user back to the login page once account is removed
+        }
+        // if user selects no, nothing else is done
+    }
+
+    private async void OnChangeMembershipClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new ManageMembership());
     }
@@ -19,6 +37,11 @@ public partial class ManageMembership : ContentPage
     public ManageMembership()
     {
        InitializeComponent();
+        var sessions = DatabaseHelper.GetSessionsByUser(App.CurrentUser.UserID);
+        firstNameLabel.Text = $"First Name: {App.CurrentUser.FirstName}";
+        lastNameLabel.Text = $"Last Name: {App.CurrentUser.LastName}";
+        birthdayLabel.Text = $"Birthday: {App.CurrentUser.Birthday.Date.ToShortDateString()}";
+        emailLabel.Text = $"Email: {App.CurrentUser.Email}";
     }
 
 }
